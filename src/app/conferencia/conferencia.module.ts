0b66340623b669fcf01e0ffae6dc1e3334bb2dba
Juan Arboleda <j.arboledam@uniandes.edu.co>
2024-05-24 14:20:01 -0500

    conferencia-list

--- src/app/conferencia/conferencia.module.ts
+++ src/app/conferencia/conferencia.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConferenciaComponent } from './conferencia.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ConferenciaListComponent } from './conferencia-list/conferencia-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule
   ],
-  declarations: [ConferenciaComponent],
-  exports:[ConferenciaComponent]
+  declarations: [ConferenciaComponent, ConferenciaListComponent],
+  exports:[ConferenciaComponent, ConferenciaListComponent]
   
 })
 export class ConferenciaModule { }
